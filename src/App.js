a47a94ed0745b1dce5876e75664d2d845e2a1465
Akshay Mahajan <akshayym94@gmail.com>
2020-08-02 15:28:47 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ClickToCopy from "react-copy-content";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+export default class App extends Component {
+  state = {
+    customSuccesss: "",
+  };
+  componentDidMount() {
+    this.mounted = true;
+  }
+
+  onCopy = (type, msg) => {
+    if (this.mounted) {
+      this.setState(
+        {
+          [type]: msg,
+        },
+        () => {
+          setTimeout(() => {
+            this.setState({
+              [type]: "",
+            });
+          }, 3000);
+        }
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="container react-copy-content">
+        <h1 className="text-center">react-copy-content</h1>
+        <p className="text-center">
+          A Fully Customizable Click-To-Copy Component for React.
+          <br />
+          <a href="https://github.com/akshayymahajan/react-copy-content">
+            Checkout the Documentation on Github
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        <section className="text-center">
+          <pre>{`
+            <ClickToCopy
+              contentToCopy="This is the default look and feel"
+            />
+          `}</pre>
+          <ClickToCopy contentToCopy="This is the default look and feel" />
+          <p>{this.state.defaultSuccess}</p>
+        </section>
+
+        <section className="text-center">
+          <pre>{`
+            <ClickToCopy
+              contentToCopy="You can use custom JSX element for the button"
+              render={props => (
+                <a href="#copy" onClick={props.copy}>
+                  Copy
+                </a>
+              )}
+            />
+          `}</pre>
+          <ClickToCopy
+            contentToCopy="You can use custom JSX element for the button"
+            render={(props) => (
+              <a href="#copy" onClick={props.copy}>
+                Copy
+              </a>
+            )}
+          />
+        </section>
+
+        <section className="text-center">
+          <pre>{`
+            <ClickToCopy
+              contentToCopy="You can even set a custom action to be triggered on copy"
+              render={props => (
+                <a href="#copy" onClick={props.copy}>
+                  Copy
+                </a>
+              )}
+            />
+          `}</pre>
+          <ClickToCopy
+            contentToCopy="You can even set a custom action to be triggered on copy"
+            onCopy={() => this.onCopy("customSuccess", "Copied Successfully!")}
+          />
+          <p>{this.state.customSuccess}</p>
+        </section>
+      </div>
+    );
+  }
+}
